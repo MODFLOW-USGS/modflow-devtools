36f5b28ffec53bc65b1354ca5c75368f3991ad08
Joseph Hughes <jdhughes@usgs.gov>
2022-07-08 15:20:45 -0500

    setup: initial files from original repo

---
+++ mfdevtools/config.py
@@ -0,0 +1,10 @@
+__author__ = "Joseph D. Hughes"
+__date__ = "March 2, 2022"
+__version__ = "0.0.1"
+__maintainer__ = "Joseph D. Hughes"
+__email__ = "jdhughes@usgs.gov"
+__status__ = "Production"
+__description__ = """
+This is the modflow-devtools package that provides functionality used in the
+MODFLOW development environment.
+"""
