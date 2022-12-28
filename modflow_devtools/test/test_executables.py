91755b804f7bcb78e83b10d421f3111726d7e874
w-bonelli <wesbonelli@gmail.com>
2022-12-28 10:27:45 -0500

    refactor(executables): simplify exes container, allow dict access

--- modflow_devtools/test/test_executables.py
+++ modflow_devtools/test/test_executables.py
@@ -4,42 +4,38 @@ from os import environ
 from pathlib import Path
 
 import pytest
-from modflow_devtools.executables import Executables
+from modflow_devtools.executables import Executables, build_default_exe_dict
 from modflow_devtools.misc import add_sys_path, get_suffixes
 
 _bin_path = Path(environ.get("BIN_PATH")).expanduser().absolute()
 _ext, _ = get_suffixes(sys.platform)
 
 
-@pytest.mark.skipif(not _bin_path.is_dir(), reason="bin directory not found")
-def test_get_path():
-    with add_sys_path(str(_bin_path)):
-        ext, _ = get_suffixes(sys.platform)
-        assert (
-            Executables.get_path("mf6", path=_bin_path)
-            == _bin_path / f"mf6{ext}"
-        )
-
-
-def test_get_version():
-    with add_sys_path(str(_bin_path)):
-        ver_str = Executables.get_version("mf6", path=_bin_path).partition(" ")
-        print(ver_str)
-        version = int(ver_str[0].split(".")[0])
-        assert version >= 6
-
-
 @pytest.fixture
 def exes():
-    return Executables(mf6=_bin_path / f"mf6{_ext}")
+    if not _bin_path.is_dir():
+        pytest.skip(f"BIN_PATH ({_bin_path}) is not a directory")
+
+    with add_sys_path(str(_bin_path)):
+        yield Executables(**build_default_exe_dict(_bin_path))
 
 
-def test_executables_mapping(exes):
+def test_get_version(exes):
+    ver_str = Executables.get_version(exes.mf6)
+    version = (
+        subprocess.check_output([f"{exes.mf6}", "-v"])
+        .decode("utf-8")
+        .split(":")[1]
+        .strip()
+    )
+    assert ver_str == version
+    assert int(ver_str[0].split(".")[0]) >= 6
+
+
+def test_mapping(exes):
     print(exes.mf6)
-    assert exes.mf6 == _bin_path / f"mf6{_ext}"
-
-
-def test_executables_usage(exes):
-    output = subprocess.check_output([f"{exes.mf6}", "-v"]).decode("utf-8")
-    print(output)
-    assert "mf6" in output
+    assert (
+        exes.mf6 == exes["mf6"]
+    )  # should support both attribute and dictionary access
+    assert exes.mf6 == _bin_path / f"mf6{_ext}"  # should be the correct path
+    assert exes.mf6_regression.parent.parent == _bin_path
