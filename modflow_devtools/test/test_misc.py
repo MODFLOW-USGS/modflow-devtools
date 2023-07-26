454cf892413a234d164b865469770d8b0f7f554d
Wes Bonelli <wpb3hw@virginia.edu>
2023-07-26 15:18:45 -0400

    test: make get_namefile_path tests less brittle

--- modflow_devtools/test/test_misc.py
+++ modflow_devtools/test/test_misc.py
@@ -184,7 +184,7 @@ def test_get_model_paths_largetestmodels():
 def test_get_model_paths_exclude_patterns(models):
     path, expected_count = models
     paths = get_model_paths(path, excluded=["gwt"])
-    assert len(paths) == expected_count
+    assert len(paths) >= expected_count
 
 
 @pytest.mark.skipif(
@@ -227,7 +227,7 @@ def test_get_namefile_paths_largetestmodels():
 def test_get_namefile_paths_exclude_patterns(models):
     path, expected_count = models
     paths = get_namefile_paths(path, excluded=["gwf"])
-    assert len(paths) == expected_count
+    assert len(paths) >= expected_count
 
 
 @pytest.mark.skipif(not any(_example_paths), reason="examples not found")
@@ -242,10 +242,10 @@ def test_get_namefile_paths_select_prefix():
 @pytest.mark.skipif(not any(_example_paths), reason="examples not found")
 def test_get_namefile_paths_select_patterns():
     paths = get_namefile_paths(_examples_path, selected=["gwf"])
-    assert len(paths) == 70
+    assert len(paths) >= 70
 
 
 @pytest.mark.skipif(not any(_example_paths), reason="examples not found")
 def test_get_namefile_paths_select_packages():
     paths = get_namefile_paths(_examples_path, packages=["wel"])
-    assert len(paths) == 43
+    assert len(paths) >= 43
