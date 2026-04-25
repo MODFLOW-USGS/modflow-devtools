720d3c7d34d698cc6a5bd38242a4d5adc976724f
mjreno <renomik@gmail.com>
2026-04-25 14:17:14 -0400

    explicitly name only first and last in block sort

--- modflow_devtools/dfns/schema/block.py
+++ modflow_devtools/dfns/schema/block.py
@@ -10,13 +10,7 @@ def block_sort_key(item) -> int:
     k, _ = item
     if k == "options":
         return 0
-    elif k == "dimensions":
-        return 1
-    elif k == "griddata":
-        return 2
-    elif k == "packagedata":
-        return 3
     elif "period" in k:
-        return 4
+        return 2
     else:
-        return 3
+        return 1
